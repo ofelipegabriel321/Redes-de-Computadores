2a4eed345eabe9974c2f911a0db39a1b9a95ce50
ofelipegabriel321 <ofelipegabriel321@gmail.com>
2019-11-25 13:34:22 -0300

    feat: adiciona método message_building_menu

--- Hamming_Code/view.py
+++ Hamming_Code/view.py
@@ -1,4 +1,12 @@
 class View:
+    def message_building_menu(self):
+        option = int(input("\n*---+++ MESSAGE BUILDING MENU +++---*"
+                           "\n| 1 - Insert new bit sequence       |"
+                           "\n| 0 - Exit                          |"
+                           "\n*-----------------------------------*"
+                           "\nInsert an option: "))
+        return option
+
     def insert_bit_sequence(self):
-        bit_sequence = input('Type the bit sequence: ')
+        bit_sequence = input("\nInsert the bit sequence: ")
         return bit_sequence

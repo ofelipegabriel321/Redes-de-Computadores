3a3e2c214f9c6b490ef720d945c63099373ea7e5
ofelipegabriel <ofelipegabriel321@gmail.com>
2019-12-04 01:06:58 -0300

    feat: adiciona método generate_bit_sequence_initial_dividend

--- Cyclic_Redundancy_Check/models.py
+++ Cyclic_Redundancy_Check/models.py
@@ -3,3 +3,9 @@ class BitSequenceHandler:
         self.initial_bit_sequence = initial_bit_sequence
         self.final_bit_sequence = None
         self.generator_polynomial = generator_polynomial
+        self.crc = None
+
+    def generate_bit_sequence_initial_dividend(self):
+        amount_of_zeros = len(self.generator_polynomial) - 1
+        self.final_bit_sequence = []
+        self.final_bit_sequence += self.initial_bit_sequence + amount_of_zeros * [0]

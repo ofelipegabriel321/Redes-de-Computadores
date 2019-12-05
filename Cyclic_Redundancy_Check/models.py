83e94f24c2b4a0e4a2481b5ab6d2cd397c60d507
ofelipegabriel321 <ofelipegabriel321@gmail.com>
2019-12-05 18:28:24 -0300

    feat: adiciona atributo crc e adequa o generate_bit_sequence_initial_dividend a ele

--- Cyclic_Redundancy_Check/models.py
+++ Cyclic_Redundancy_Check/models.py
@@ -1,11 +1,11 @@
+from copy import deepcopy
+
 class BitSequenceHandler:
     def __init__(self, initial_bit_sequence, generator_polynomial):
         self.initial_bit_sequence = initial_bit_sequence
         self.final_bit_sequence = None
         self.generator_polynomial = generator_polynomial
-        self.crc = None
+        self.crc = [0] * (len(self.generator_polynomial) - 1)
 
     def generate_bit_sequence_initial_dividend(self):
-        amount_of_zeros = len(self.generator_polynomial) - 1
-        self.final_bit_sequence = []
-        self.final_bit_sequence += self.initial_bit_sequence + amount_of_zeros * [0]
+        self.final_bit_sequence = self.initial_bit_sequence + self.crc

423b11a3f1d77a5f51ef4e09cf69b81a0d5c4b5f
ofelipegabriel321 <ofelipegabriel321@gmail.com>
2019-12-05 22:16:24 -0300

    feat: adiciona método color_text e adequa alguns métodos a ele

--- Cyclic_Redundancy_Check/view.py
+++ Cyclic_Redundancy_Check/view.py
@@ -1,11 +1,22 @@
 class View:
     
+    def color_text(self, color):
+        color_palette = {"standart color": "\033[0m",
+                         "red": "\033[0;31;40m",
+                         "green": "\033[0;32;40m",
+                         "yellow": "\033[0;33;40m",
+                         "white": "\033[0;37;40m",
+                         "white with underline": "\033[4;37;40m"}
+        return color_palette[color]
+    
     def message_building_menu(self):
-        option = input("\n*----------------+++ MESSAGE BUILDING MENU +++----------------*"
+        option = input(self.color_text("yellow") + ""
+                       "\n*----------------+++ MESSAGE BUILDING MENU +++----------------*"
                        "\n| 1 - Insert new bit sequence and generator                   |"
                        "\n| 0 - Exit                                                    |"
                        "\n*-------------------------------------------------------------*"
-                       "\nInsert an option: ")
+                       "\nInsert an option: "
+                       "" + self.color_text("standart color"))
         return option
     
     def insert_bit_sequence(self):

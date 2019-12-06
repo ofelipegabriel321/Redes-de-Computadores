8e9a219d1cb34569280e3cb078bad06f6224f4d1
ofelipegabriel321 <ofelipegabriel321@gmail.com>
2019-12-05 21:42:28 -0300

    feat: adiciona message_building_menu

--- Cyclic_Redundancy_Check/view.py
+++ Cyclic_Redundancy_Check/view.py
@@ -1,4 +1,13 @@
 class View:
+    
+    def message_building_menu(self):
+        option = input("\n*----------------+++ MESSAGE BUILDING MENU +++----------------*"
+                       "\n| 1 - Insert new bit sequence and generator                   |"
+                       "\n| 0 - Exit                                                    |"
+                       "\n*-------------------------------------------------------------*"
+                       "\nInsert an option: ")
+        return option
+    
     def insert_bit_sequence(self):
         bit_sequence = input("\nInsert the bit sequence: ")
         return bit_sequence
